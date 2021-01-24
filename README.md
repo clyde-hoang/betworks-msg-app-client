500926a532aa92ef01f7808534a7331779bc3991
Clyde Hoang <choang008@gmail.com>
2021-01-24 12:54:22 -0800

    Cleaned up some code

--- README.md
+++ README.md
@@ -2,17 +2,19 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.5. The project is used to demonstrate some core competencies in front end web develpoment.
 
+## Installing Docker
+Follow the following [link](https://www.docker.com/get-started) to install docker
+
 ## Building the Application
-Run `docker build -t betworkclient .` to build the project.
+Run `docker build -t msgclient .` to build the project.
 
 ## Run the application
-Run `docker run -it -p 80:80 --rm betworkclient` to run the project on port 80.
-navigate to `[machine_ip]:80` to access the web application
+Run `docker run -it -p 80:80 --rm msgclient` to run the project on port 80.
+navigate to `[machine_ip]:80` or `[machine_ip]` ex. http://localhost to access the web application
+<br>
+Some valid test users are: <br> 
+`user:test password:test` <br>
+`user:test2 password:test`
 
 ## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+Run `npm run test` to execute the unit tests via [Karma](https://karma-runner.github.io).
--- src/app/store/authentication/reducers/auth.reducer.spec.ts
+++ src/app/store/authentication/reducers/auth.reducer.spec.ts
@@ -21,7 +21,7 @@ describe('Auth Reducer Test', () => {
 
   describe('LOGIN', () => {
     it('should set the correct states', () => {
-      const action: any = new fromActions.LoginAction({ email: 'email', password: 'password' });
+      const action: any = new fromActions.LoginAction({ username: 'email', password: 'password' });
       const state = fromReducer.authReducer(defaultAuthState, action);
 
       expect(state.loading).toEqual(true);
@@ -43,57 +43,6 @@ describe('Auth Reducer Test', () => {
   describe('LOGIN_ERROR', () => {
     it('should set the correct states', () => {
       const action: any = new fromActions.LoginErrorAction({ error });
-      const state = fromReducer.authReducer(defaultAuthState, action);
-
-      expect(state.loading).toEqual(false);
-      expect(state.loaded).toEqual(false);
-      expect(state.error).toEqual(error as any);
-    });
-  });
-
-  describe('TWO_FA_LOGIN', () => {
-    it('should set the correct states', () => {
-      const action: any = new fromActions.TwoFALogin({ email: 'email', password: 'password', authenticationCode: '12345' });
-      const state = fromReducer.authReducer(defaultAuthState, action);
-
-      expect(state.loading).toEqual(true);
-      expect(state.loaded).toEqual(false);
-      expect(state.error).toEqual(null);
-    });
-  });
-  describe('TWO_FA_LOGIN_FAIL', () => {
-    it('should set the correct states', () => {
-      const action: any = new fromActions.TwoFALoginFail({ error });
-      const state = fromReducer.authReducer(defaultAuthState, action);
-
-      expect(state.loading).toEqual(false);
-      expect(state.loaded).toEqual(false);
-      expect(state.error).toEqual(error as any);
-    });
-  });
-
-  describe('RESEND_AUTHENTICATION_CODE', () => {
-    it('should set the correct states', () => {
-      const action: any = new fromActions.ResendAuthenticationCode('email');
-      const state = fromReducer.authReducer(defaultAuthState, action);
-
-      expect(state.loading).toEqual(true);
-      expect(state.loaded).toEqual(false);
-      expect(state.error).toEqual(null);
-    });
-  });
-  describe('RESEND_AUTHENTICATION_CODE_SUCCESS', () => {
-    it('should set the correct states', () => {
-      const action: any = new fromActions.LoginSuccessfulAction({ token: 'TOKEN' });
-      const state = fromReducer.authReducer(defaultAuthState, action);
-
-      expect(state.loading).toEqual(false);
-      expect(state.error).toEqual(null);
-    });
-  });
-  describe('RESEND_AUTHENTICATION_CODE_FAIL', () => {
-    it('should set the correct states', () => {
-      const action: any = new fromActions.ResendAuthenticationCodeFail({ error });
       const state = fromReducer.authReducer(defaultAuthState, action);
 
       expect(state.loading).toEqual(false);
